28f59facda2485cccbd2b9c41f358f48b9a25dd6
amirhosein <amirhosein7590@gmail.com>
2026-09-14 20:23:21 +0330

    add some enviorment variables and global config

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+export const SCROLL_LENGTH = '1100vh'
+export const CAMERA_FOV = 38
+export const SCROLL_SCRUB = 2.0
+export const DURATION_ENTRANCE = 2.2
+export const HERO_TEXT_DELAY = 0.5
+
+export const WATCH_MODEL_PATH = '/watch-3d-model.glb'
+export const DRACO_DECODER_PATH =
+  'https://www.gstatic.com/draco/versioned/decoders/1.5.6/'
